be95d5c02bc04421afbc55223eac66d6c8aab84b
sowmiya623 <110320161+sowmiya623@users.noreply.github.com>
2022-10-31 15:14:27 +0530

    Add files via upload

---
+++ positivepy.py
@@ -0,0 +1,12 @@
+# Python program to print positive Numbers in given range
+
+
+list1 = [-14, -12,64, 45, 5, -93]
+  
+# iterating each number in list
+
+for num in list1:
+      
+    # checking condition
+    if num >= 0:     
+       print(num, end = " ")
